bba00218234281406aa6def480371c29f16a4519
eddymens <kwasiamantin@gmail.com>
2016-09-10 23:31:13 +0000

    obey psr2

--- example/server.php
+++ example/server.php
@@ -1,11 +1,11 @@
 <?php
   require('../json-rpc.php');
-  class SampleClass {
-    public function index($name) {
-      return "Hello ".$name;
+class SampleClass
+{
+    public function index($name)
+    {
+        return "Hello ".$name;
     }
-  }
+}
 
   handle_json_rpc(new SampleClass());
- 
- ?>
