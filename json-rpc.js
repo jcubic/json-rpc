f4a6e436dad003a4b4cb1b01676671ae5e01333f
eddymens <kwasiamantin@gmail.com>
2016-09-10 23:20:10 +0000

    add example to index

--- json-rpc.js
+++ json-rpc.js
@@ -8,28 +8,30 @@
 
 var jsonrpc = {
     
-  call: function(url, method, params) {
+  call: function(url, method, params, callback) {
     var data = JSON.stringify({
         "version": 1.1,
         "method": method,
         "params": params,
         "id": this.getId(1, 10000)
       });
-    this.request(url, data);
+    this.request(url, data, callback);
   },
 
   getId: function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
  
-  request: function(url, data) {
+  request: function(url, data, callback) {
          
       var xhr = new XMLHttpRequest();
       xhr.withCredentials = true;
 
       xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-          console.log(this.responseText);
+          response = JSON.parse(this.responseText)
+          callback(response)
+          
         }
       });
 
